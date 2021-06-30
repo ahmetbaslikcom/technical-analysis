139c9c71c0ea7caf6afbcf81e243905a67d94cb5
Ahmet Başlık <60074329+ahmetbaslikcom@users.noreply.github.com>
2021-07-01 01:22:22 +0300

    Create rsi.py

---
+++ rsi.py
@@ -0,0 +1,16 @@
+def rsi(veri,period): # veri-> closing values
+    delta = veri.diff().dropna()
+    u = delta * 0
+    d = u.copy()
+    u[delta > 0] = delta[delta > 0]
+    d[delta < 0] = -delta[delta < 0]
+    u[u.index[period - 1]] = np.mean(u[:period])  # first value is sum of avg gains
+    u = u.drop(u.index[:(period - 1)])
+    d[d.index[period - 1]] = np.mean(d[:period])  # first value is sum of avg losses
+    d = d.drop(d.index[:(period - 1)])
+    rs = pd.DataFrame.ewm(u, com=period - 1, adjust=False).mean() / \
+         pd.DataFrame.ewm(d, com=period - 1, adjust=False).mean()
+
+    rsi=100 - 100 / (1 + rs)
+
+    return rsi
